9a112e8aa2e6e0b95e04ed7946620b17c28892cc
Rajibbepary <rajib635356@gmail.com>
2026-04-13 11:46:16 +0600

    interview number one

---
+++ index.js
@@ -0,0 +1,177 @@
+
+
+//function myFun () {return 'Hello world'}
+
+// function myFun (){
+//     return 'Hello world'
+// }
+
+// const myFun = (x, y) =>{
+//     return x + y
+// }
+//onsole.log(myFun(10 ,20))
+
+// function sayHello (name){
+//     return 'Rajib' + name
+// }
+
+// let fullName = sayHello('raj')
+
+// console.log(fullName)
+
+// function myMap(arr, callback) {
+//   let result = [];
+  
+//   for (let i = 0; i < arr.length; i++) {
+//     result.push(callback(arr[i], i));
+//   }
+
+//   return result;
+// }
+
+// // test
+// console.log(myMap([1,2,3], x => x * 2)); // [2,4,6]
+
+
+// console.log(typeof "Hello");   // string
+// console.log(typeof 10);        // number
+// console.log(typeof true);      // boolean
+// console.log(typeof undefined); // undefined
+// console.log(typeof null);      // object (JavaScript bug)
+// console.log(typeof [1,2,3]);   // object
+// console.log(typeof {});        // object
+// console.log(typeof function(){}); // function
+// console.log( +"123abc");
+//console.log(typeof NaN)
+//console.log ( ''== 0)
+
+//let x = 5;
+//console.log (x != 5)
+
+// let userName = '';
+
+// let name = userName && 'Rajib';
+
+// let age = 10;
+
+// let result = age < 18 ? 'Adult' : 'Child';
+
+// console.log(result)
+//const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+
+// const carobject = cars.map((car, index) =>{
+//     return {
+//         car,
+//         index
+//     }
+// })
+
+// const carobject = cars.map((car, index) =>({
+//     id: index,
+//     name:car
+// }))
+
+
+// const html = cars.map (car => `<li>${car}</li>`);
+// console.log(html.join(' '));
+//console.log(carobject.join );
+
+ //let text = ' ';
+//let i = 0;
+
+//et len = cars.length; break()
+
+// 
+
+// for(let i = 0; i < 10 ; i++){
+//     if( i===4){continue}
+//     text += "Hello" + i;
+// }
+
+
+
+
+// for (let i = 0; i < 10; i++){
+//     text += "Hello" + [i] + ' ' ;
+// }
+
+
+
+
+
+
+
+
+// let i = 0;
+// while(i < 10){
+//     text += 'Hello' + [i] + ' ';
+//     i++;
+// }
+
+
+//Today my topic discuse what is a javaScript string method
+
+//let text = 'Hello World';
+
+
+//let num = '1290pxe';
+
+//let nub = num.toString()
+
+//console.log(typeof num)
+//console.log(num)
+
+// function calculateTotal(price, quantity, taxRate, discount) {
+//   let subtotal = price * quantity;
+//   let tax = subtotal * taxRate;
+//   let total = subtotal + tax - discount;
+
+//   return total.toFixed(2);
+// }
+
+// // test
+// console.log(calculateTotal(100, 2, 0.1, 40)); // "200.00"
+
+
+//const arr = [100, 212, 432, 432];
+
+//const ans = arr.reduce((acc, curr)=> acc + curr);
+
+// const arr = [11, 'a', 'd', 5, 9]
+
+// const numArr = arr.filter((curr)=> typeof curr === typeof 1)
+
+// console.log(numArr)
+
+
+//JavaScript interview qustion
+
+// console.log([]==![]);
+
+// function myFun (){
+//     return
+//     {name: 'Rajib'}
+// }
+// console.log(myFun());
+
+// the difference between forEach and map method ?
+// the difference between filter and find method ?
+// the difference between some method and every method ?
+// the difference between primitive  and non-primitive data types ?
+// function is a primitive and non-primitive data type ?
+// what is what do you mean by selfinvoking functions ?
+// What do you mean by anonymous functions ?
+// what is the difference between call apply  and bind methods ?
+// there are diffrent types of scopes in JavaScript which are the ?
+// what are callback functions ?
+// there are different types of errors ?
+// what is a NaN property ?
+// What is a function curring in JavaScript ?
+// Can you tell me about JSX ?
+// What is higer order components ?
+// About coustom hooks ?
+// What is the use of lazy loading ?
+// What is a react router ?
+// The difference between  Redux and  context API ?
+
